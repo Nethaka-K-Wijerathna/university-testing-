5126cbe5fb8dbb1ea72c5ac7246168b10333382b
Nethaka Kaveen Wijerathna <nethaka2003@gmail.com>
2025-01-31 18:34:18 +0530

    Create var.c
    new

---
+++ var.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+   char var1;
+   printf("Enter the value:");
+   scanf("%c",&var1);
+   printf("value is :%d\n",var1);
+   printf("value is :%c",var1);
+  
+  char var2;
+   printf("\nEnter the value:");
+   scanf("%d",&var2);
+   printf("value is :%d \n",var2);
+   printf("value is :%c",var2);
+    return 0;
+}
