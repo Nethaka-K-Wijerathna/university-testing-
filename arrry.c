69d589dd6e5841235aaabf63e390122cfd40f20a
Nethaka Kaveen Wijerathna <nethaka2003@gmail.com>
2025-01-31 15:00:56 +0530

    Add files via upload
    test

---
+++ arrry.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char var1[100]
+    printf("Enter the value:");
+   scanf("%s"&var1)
+   printf("value is :%s\n",var1);
+   printf("value is :%c",var1);
+  
+
+    return 0;
+}
