29e406784c100f71a256b70edaea25111156455f
Nethaka Kaveen Wijerathna <nethaka2003@gmail.com>
2025-01-31 15:45:46 +0530

    Update arrry.c
    done

--- arrry.c
+++ arrry.c
@@ -3,7 +3,7 @@ int main(int argc, char const *argv[])
 {
     char var1[100]
     printf("Enter the value:");
-   scanf("%s"&var1)
+   scanf("%s",&var1);
    printf("value is :%s\n",var1);
    printf("value is :%c",var1);
   
