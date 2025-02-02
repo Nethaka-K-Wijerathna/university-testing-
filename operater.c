989cfb14727552d65707594e1ce277c455b1a030
Nethaka Kaveen Wijerathna <nethaka2003@gmail.com>
2025-02-02 10:55:21 +0530

    Create operater.c
    add a file

---
+++ operater.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=9,b=4,result;
+    result = a/b;
+    printf("%d \n",result);
+    result = a-b;
+    printf("%d \n",result);
+    result = a+b;
+    printf("%d \n",result);
+     result = a<b;
+    printf("%d \n",result);
+     result = a==b;
+    printf("%d \n",result);
+     result = a>=b;
+    printf("%d \n",result); 
+    result = a!=b;
+    printf("%d \n",result);
+    return 0;
+}
